2195ac0d392acae88691dbdb0356251300e93c52
Asher Huang <whuang@onlyou.com>
2020-05-30 12:33:35 +0800

    第3个内部类练习

---
+++ src/main/java/ThinkingInJavaPractice/InnerClasses/Sequence.java
@@ -0,0 +1,62 @@
+package ThinkingInJavaPractice.InnerClasses;
+
+/**
+ * @Author:asher
+ * @Date:2020/5/30 12:17
+ * @Description:ThinkingInJavaPractice.InnerClasses
+ * @Version:1.0
+ */
+interface Selector {
+    boolean end();
+
+    Object current();
+
+    void next();
+}
+public class Sequence {
+   private Object[] items;
+    private int next = 0;
+
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+
+    public void add(Object x) {
+        if (next < items.length) {
+            items[next++]=x;
+        }
+    }
+
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+
+        public boolean end() {
+            return i == items.length;
+        }
+
+        public Object current() {
+            return items[i];
+        }
+
+        public void next() {
+            if (i < items.length) {
+                i++;
+            }
+        }
+    }
+
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(10);
+        for (int i = 0; i < 10; i++) {
+            sequence.add(Integer.toString(i));
+        }
+        Selector selector = sequence.selector();
+        while (!selector.end()) {
+            System.out.printf(selector.current() + " ");
+            selector.next();
+        }
+    }
+}
