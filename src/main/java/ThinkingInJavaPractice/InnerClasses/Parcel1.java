7139f83a795f18e053ab1567a063f2dc90571db7
Asher Huang <whuang@onlyou.com>
2020-05-30 09:39:17 +0800

    第一个内部类练习

---
+++ src/main/java/ThinkingInJavaPractice/InnerClasses/Parcel1.java
@@ -0,0 +1,39 @@
+package ThinkingInJavaPractice.InnerClasses;
+
+/**
+ * @Author:asher
+ * @Date:2020/5/30
+ * @Description:ThinkingInJavaPractice.InnerClasses
+ * @Version:1.0
+ */
+public class Parcel1 {
+    class Contents{
+        private int i = 11;
+
+        public int value() {
+            return i;
+        }
+    }
+    class Destination{
+        private String label;
+
+        Destination(String whereTo) {
+            label = whereTo;
+        }
+
+        String readLabel() {
+            return label;
+        }
+    }
+
+    public void ship(String dest) {
+        Contents contents = new Contents();
+        Destination destination = new Destination(dest);
+        System.out.println(destination.readLabel());
+    }
+
+    public static void main(String[] args) {
+        Parcel1 parcel1 = new Parcel1();
+        parcel1.ship("Tasmania");
+    }
+}
---
+++ src/main/java/ThinkingInJavaPractice/InnerClasses/Parcel2.java
@@ -0,0 +1,10 @@
+package ThinkingInJavaPractice.InnerClasses;
+
+/**
+ * @Author:asher
+ * @Date:2020/5/30
+ * @Description:ThinkingInJavaPractice.InnerClasses
+ * @Version:1.0
+ */
+public class Parcel2 {
+}
