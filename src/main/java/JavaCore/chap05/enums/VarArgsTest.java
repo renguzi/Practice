9e49206e4c676eca76f6147fcfcd6ec8c553a6af
Asher Huang <whuang@onlyou.com>
2020-06-14 16:50:20 +0800

    一个关于可变参数的例子。方法中的所有可变参数，都可以用数组替代。其实，编译器就是这么干的，在其遇到可变参数时，都自动将其转换成数组，new Object[]{...}。

---
+++ src/main/java/JavaCore/chap05/enums/VarArgsTest.java
@@ -0,0 +1,24 @@
+package JavaCore.chap05.enums;
+
+/**
+ * @Author:asher
+ * @Date:2020/6/14 16:41
+ * @Description:JavaCore.chap05.enums
+ * @Version:1.0
+ */
+public class VarArgsTest {
+    public static void main(String... args) {
+        double[] darray = new double[]{3.14, -1.24, 56.2, 901.3};
+        System.out.println("The max element of the array is" + maxDouble(darray));
+    }
+
+    public static double maxDouble(double... doubles) {
+        double d = Double.NEGATIVE_INFINITY;
+        for (double d1:doubles) {
+            if (d1 > d) {
+                d=d1;
+            }
+        }
+        return d;
+    }
+}
