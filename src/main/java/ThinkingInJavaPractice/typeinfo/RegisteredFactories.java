fb8f6e2e4958796c2c6bb0198b08d88d9557ed01
Asher Huang <whuang@onlyou.com>
2020-07-05 11:00:59 +0800

    Registered Factory.

---
+++ src/main/java/ThinkingInJavaPractice/typeinfo/RegisteredFactories.java
@@ -0,0 +1,111 @@
+package ThinkingInJavaPractice.typeinfo;
+
+import ThinkingInJavaPractice.typeinfo.factory.Factory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @Author:asher
+ * @Date:2020/7/5 09:59
+ * @Description:ThinkingInJavaPractice.typeinfo
+ * @Version:1.0
+ */
+class Part {
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+    static List<Factory<? extends Part>> partFactories = new ArrayList<Factory<? extends Part>>();
+    static {
+        partFactories.add(new FuelFilter.Factory());
+        partFactories.add(new AirFilter.Factory());
+        partFactories.add(new CabinAirFilter.Factory());
+        partFactories.add(new OilFilter.Factory());
+        partFactories.add(new FanBelt.Factory());
+        partFactories.add(new PowerSteeringBelt.Factory());
+        partFactories.add(new GeneratorBelt.Factory());
+
+    }
+
+    private static Random rand = new Random(47);
+    public static Part createRandom() {
+        int n = rand.nextInt(partFactories.size());
+        return partFactories.get(n).create();
+    }
+}
+
+class Filter extends Part {
+}
+
+class FuelFilter extends Filter {
+    public static class Factory implements ThinkingInJavaPractice.typeinfo.factory.Factory<FuelFilter> {
+        public FuelFilter create() {
+            return new FuelFilter();
+        }
+    }
+}
+
+class AirFilter extends Filter {
+    public static class Factory implements ThinkingInJavaPractice.typeinfo.factory.Factory<AirFilter> {
+        @Override
+        public AirFilter create() {
+            return new AirFilter();
+        }
+    }
+}
+
+class CabinAirFilter extends Filter {
+    public static class Factory implements ThinkingInJavaPractice.typeinfo.factory.Factory<CabinAirFilter> {
+        @Override
+        public CabinAirFilter create() {
+            return new CabinAirFilter();
+        }
+    }
+}
+
+class OilFilter extends Filter {
+    public static class Factory implements ThinkingInJavaPractice.typeinfo.factory.Factory<OilFilter> {
+        @Override
+        public OilFilter create() {
+            return new OilFilter();
+        }
+    }
+}
+
+class Belt extends Part {
+}
+
+class FanBelt extends Belt {
+    public static class Factory implements ThinkingInJavaPractice.typeinfo.factory.Factory<FanBelt> {
+        @Override
+        public FanBelt create() {
+            return new FanBelt();
+        }
+    }
+}
+
+class GeneratorBelt extends Belt {
+    public static class Factory implements ThinkingInJavaPractice.typeinfo.factory.Factory<GeneratorBelt> {
+        @Override
+        public GeneratorBelt create() {
+            return new GeneratorBelt();
+        }
+    }
+}
+
+class PowerSteeringBelt extends Belt {
+    public static class Factory implements ThinkingInJavaPractice.typeinfo.factory.Factory<PowerSteeringBelt> {
+        @Override
+        public PowerSteeringBelt create() {
+            return new PowerSteeringBelt();
+        }
+    }
+}
+public class RegisteredFactories {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Part.createRandom());
+        }
+    }
+}
