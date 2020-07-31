92e6bd5a412d9cc646f20d86da22231904612fe8
Asher Huang <whuang@onlyou.com>
2020-07-31 20:40:05 +0800

    第一个JavaIO练习.判断文件是否存在以及创建文件。

---
+++ src/main/java/com/imooc/IO/JavaIODemo1.java
@@ -0,0 +1,31 @@
+package com.imooc.IO;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @Author:asher
+ * @Date:2020/7/31 20:19
+ * @Description:com.imooc.IO
+ * @Version:1.0
+ */
+public class JavaIODemo1 {
+    public static void main(String[] args) {
+//        File file1 = new File("/Users/asher/imooc/imooc2.txt");
+//        File file1 = new File("/Users/asher", "/imooc/imooc1.txt");
+        File file = new File("/Users/asher");
+        File file1 = new File(file, "/imooc/imooc1.txt");
+        System.out.println(file1.isDirectory());
+        System.out.println(file1.isFile());
+
+        File file2 = new File("/Users/asher/imooc/tz");
+        if (!file2.exists()) {
+            try {
+                file2.createNewFile();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+}
