1da0ea75bc8e199c2bcd72931408c1bbfdf73599
Asher Huang <whuang@onlyou.com>
2020-09-10 11:17:50 +0800

    冒泡排序增加标志位。

--- src/main/java/cn/how2j/JavaBasic/Array/BubbleSort.java
+++ src/main/java/cn/how2j/JavaBasic/Array/BubbleSort.java
@@ -18,12 +18,17 @@ public class BubbleSort {
         }
 
         for (int i = 0; i < array.length-1; i++) {
+            //加入标志位，用于判断是否还需要继续冒泡，对于有些情况下，数据已经是有序的，就不再需要排序了
+            boolean flag = false;
             for (int j = i+1; j < array.length; j++) {
                 if (array[i] < array[j]) {
                     int temp = array[j];
                     array[j] = array[i];
                     array[i] = temp;
+                    flag = true;
                 }
+                if (!flag)
+                    break;
             }
         }
         System.out.println("After Bubble sorted:");
---
+++ src/main/java/cn/how2j/JavaBasic/Array/SelectionTest.java
@@ -0,0 +1,17 @@
+package cn.how2j.JavaBasic.Array;
+
+/**
+ * @Author:asher
+ * @Date:2020/9/10 11:12
+ * @Description:cn.how2j.JavaBasic.Array
+ * @Version:1.0
+ */
+public class SelectionTest {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 100);
+        }
+
+    }
+}
