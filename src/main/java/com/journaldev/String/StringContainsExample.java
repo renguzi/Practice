c44407801f03038e879b1bc4dd6a28cd0e7e2b53
Asher Huang <whuang@onlyou.com>
2020-09-18 16:52:55 +0800

    String的contains()方法测试。

---
+++ src/main/java/com/journaldev/String/StringContainsExample.java
@@ -0,0 +1,15 @@
+package com.journaldev.String;
+
+/**
+ * @Author:asher
+ * @Date:2020/9/18 16:52
+ * @Description:com.journaldev.String
+ * @Version:1.0
+ */
+public class StringContainsExample {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.contains("W"));
+        System.out.println(s.contains("X"));
+    }
+}
